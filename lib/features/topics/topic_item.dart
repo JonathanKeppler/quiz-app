09a6878136957ae160ff6ccf183869511c5ae4f1
Jonathan Keppler <JKeppler27@gmail.com>
2022-03-30 09:42:41 -0400

    feat(auth): add google auth

---
+++ lib/features/topics/topic_item.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TopicItem extends StatelessWidget {
+  const TopicItem({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/shared/error.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ErrorMessage extends StatelessWidget {
+  final String message;
+
+  const ErrorMessage({
+    Key? key,
+    this.message = 'It Broke!'
+    }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(message),      
+    );
+  }
+}
